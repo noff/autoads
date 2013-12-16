01702455b6b29577a94d4074a6a1a6e4a87fa689
Michael Kechinov <sam@mkechinov.ru>
2013-12-16 19:59:04 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Autoads::Application.config.secret_key_base = 'd8affddfdf463cb5d8025267ee550d89d9c7d4c9f5437188b85daac66e01e70f8232c3a6760a519660e4a436185c1f159e06a907d58a721dfa9afff2abd97033'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Autoads::Application.config.session_store :cookie_store, key: '_autoads_session'
