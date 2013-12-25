03ec9a1c2102abd2fd76f182bfc1c1b8d20d2efb
Michael Kechinov <sam@mkechinov.ru>
2013-12-25 09:52:09 +0400

    Конфиги для mixpanel

--- app/services/mixpanel_api.rb
+++ app/services/mixpanel_api.rb
@@ -1,10 +1,29 @@
 class MixpanelAPI
 
-  @@api_key = MIXPANEL_CONFIG['mixpanel_api_key']
-  @@api_secret = MIXPANEL_CONFIG['mixpanel_api_secret']
+  require 'rubygems'
+  require 'mixpanel_client'
+
+  config = {api_key: MIXPANEL_CONFIG['mixpanel_api_key'], api_secret: MIXPANEL_CONFIG['mixpanel_api_secret']}
+  @@client = Mixpanel::Client.new(config)
 
   def self.get_funnel
-    Rails.logger.info @@api_key
+    data = @@client.request('funnels', {
+        #from_date: 1.month.ago.strftime('"%Y-%m-%d'),
+        #to_date: Date.current.strftime('"%Y-%m-%d'),
+        expire: (DateTime.current + 1.hour).to_i,
+        funnel_id: MIXPANEL_CONFIG['funnel_id'],
+        where: "properties[\"utm_campaign\"] == \"Buffer\"",
+        unit: 'month'
+    })
+    data
+  end
+
+
+  def self.get_funnels
+    data = @@client.request('funnels/list', {
+        expire: (DateTime.current + 1.hour).to_i
+    })
+    data
   end
 
 end
